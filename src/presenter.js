09db9e069a9e562dd528afc00a1537c10e12d6b5
Jacco126 <jonatan.terceros@ucb.edu.bo>
2023-03-21 00:28:52 -0400

    introducir numero y representarlo en romano (interfaz de usuario)

--- src/presenter.js
+++ src/presenter.js
@@ -1,33 +1,11 @@
-import sumar from "./sumador";
-import multiplicar from "./multiplicador";
+import romano from "./romanos";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const num = document.querySelector("#numero");
+const form = document.querySelector("#romanos-form");
 const div = document.querySelector("#resultado-div");
-
-const first_m = document.querySelector("#primer-numero-m");
-const second_m = document.querySelector("#segundo-numero-m");
-const form2 = document.querySelector("#multiplicar-form");
-const div_mult = document.querySelector("#resultado-div-mult")
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
-
-
-});
-
-form2.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  const firstNumber = Number.parseInt(first_m.value);
-  const secondNumber = Number.parseInt(second_m.value);
-
-  div_mult.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
-
+  div.innerHTML = "El numero representado en romano es: <p>" + romano(num.value) + "</p>";
 });
